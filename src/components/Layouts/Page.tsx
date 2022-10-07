d68626b26e9938e8a454745ad6871c4f7aa53928
baopham <pqbao27@gmail.com>
2022-10-08 06:15:43 +0700

    update components

---
+++ src/components/Layouts/Page.tsx
@@ -0,0 +1,38 @@
+import { Paper, PaperProps, Typography } from '@mui/material'
+import React from 'react'
+
+import { backgroundColor } from '@/styles'
+
+type PageProps = {
+  title?: string
+} & PaperProps
+
+export const Page = ({ title, children, ...props }: PageProps) => {
+  return (
+    <Paper
+      // elevation={1}
+      sx={{
+        padding: '30px 20px',
+        margin: '8px 0',
+        borderRadius: 2,
+        backgroundColor: backgroundColor['primary'],
+      }}
+      {...props}
+    >
+      {title && (
+        <Typography
+          variant="h5"
+          sx={{
+            fontWeight: 700,
+            fontSize: { xs: '1rem', md: '1.5rem' },
+            color: '#fff',
+          }}
+          color="grey.900"
+        >
+          {title}
+        </Typography>
+      )}
+      {children}
+    </Paper>
+  )
+}
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,80 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { Column } from 'react-table'
+
+import { ReactTableWithToolBar } from '@/components/ReactTable'
+
+type ColType = {
+  id: string
+  market_cap_rank: number
+  name: string
+  current_price: string
+  price_change_percentage_24h: string
+  market_cap: string
+  circulating_supply: string
+}
+
+const endpoint = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
 
 export const Home = () => {
-  return <div>Home</div>
+  // const [params, setParams] = React.useState({ _page: 0, _limit: 5 })
+  const [params, setParams] = React.useState({})
+
+  // const { paginationData, handleChangeParams, refetch } = usePaginationQuery<any>(endpoint, params)
+
+  const { isLoading, error, data, refetch, isSuccess } = useQuery<ColType[]>(
+    [endpoint, { ...params }],
+    {
+      keepPreviousData: true,
+    },
+  )
+
+  console.log({ data })
+
+  const columns = React.useMemo<Column<ColType>[]>(
+    () => [
+      {
+        Header: '#',
+        accessor: 'market_cap_rank', // accessor is the "key" in the data
+        width: 10,
+      },
+      {
+        Header: 'name',
+        accessor: 'name',
+        width: 200,
+      },
+      {
+        Header: 'price',
+        accessor: 'current_price',
+        width: 200,
+      },
+      {
+        Header: 'price_change_percentage_24h',
+        accessor: 'price_change_percentage_24h',
+      },
+      {
+        Header: 'market_cap',
+        accessor: 'market_cap',
+      },
+      {
+        Header: 'circulating_supply',
+        accessor: 'circulating_supply',
+      },
+    ],
+    [],
+  )
+
+  return (
+    <div>
+      <ReactTableWithToolBar
+        title="Top các loại tiền điện tử theo khối lượng"
+        columns={columns}
+        data={data || []}
+        // handleChangeParams={handleChangeParams}
+        // {...paginationData}
+        // pageCount={10}
+        // manualPagination={true}
+      />
+    </div>
+  )
 }
